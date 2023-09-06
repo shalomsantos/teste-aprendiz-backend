90b9fadd19b8269966c32fd9dfcfc31d6590f2b7
Shalomsantos <shalomsantos1234@gmail.com>
2023-09-06 17:34:42 -0300

    trabalhando na função edit

--- edit.php
+++ edit.php
@@ -27,8 +27,8 @@
                 $pdoInstance = $conexao->getConnection();
 
                 // ações de crud
-                if(!empty($_GET['id'])){
-                    $id = $_GET['id'];
+                if(!empty($_GET['editar'])){
+                    $id = $_GET['editar'];
 
                     $sql = "SELECT * FROM usuario WHERE id=$id";
                     $result = $pdoInstance->prepare($sql);
@@ -66,10 +66,6 @@
                     }
                 }
                 // ações de crud
-                if(isset($_GET['delete'])){
-                    $id = (int)$_GET['delete'];
-                    $pdoInstance->exec("DELETE FROM usuario WHERE id=$id");
-                }
                 if(isset($_GET['editar'])){
 
                     $id = (int)$_GET['editar'];
@@ -90,7 +86,7 @@
                 }
             ?>
         </span>
-        <form action="index.php" method="POST">
+        <form action="edit.php" method="POST">
             <h1 class="text-center">Adicionar usuário</h1>
             <div class="col-6 container d-flex p-2">
                 <div class="col-6 d-flex flex-column me-2" style="gap: 1rem;">
@@ -102,54 +98,12 @@
                     <input type="password" name="password" id="password" class="form-control" placeholder="Senha" required value="<?php if(isset($result_user['senha'])){echo $result_user['senha'];} ?>">
                     <input type="password" name="confirmPassword" id="confirmPassword" class="form-control" placeholder="Senha confirm:" required value="<?php if(isset($result_user['senha'])){echo $result_user['senha'];} ?>">
                     <div>
-                        <button class="btn btn-outline-success" type="submit" name="submit" id="submit" onclick="SenhaConfirm(event)">Adicionar</button>
                         <button class="btn btn-outline-primary" type="submit" name="editar" id="editar">Editar</button>
                         <button class="btn btn-outline-primary" type="button" onclick="Clean()" id="limpar"><i class="fa-solid fa-broom"></i></button>
                     </div>
                 </div>
             </div>
         </form>
-
-        <table>
-            <theade>
-                <tr>
-                    <th>Nº</th>
-                    <th>Nome</th>
-                    <th>E-mail</th>
-                    <th>Telefone</th>
-                    <th>...</th>
-                </tr>
-            </theade>
-            <tbody>
-                <tr>
-                    <?php
-                        //realizando consulta(retornando tudo) na instancia aberta anteriormente
-                        $sql = $pdoInstance->prepare('SELECT * FROM usuario');
-                        $sql->execute();
-                        $fechUsuarios = $sql->fetchAll();
-
-                        //se ouver registro neste a linha será populada com os dados
-                        if ($fechUsuarios) {
-                            $count = 1;
-                            foreach ($fechUsuarios as $row) {
-                                echo '<tr>';
-                                echo '<td>'.$row['id'].'</td>';
-                                echo '<td>'.$row['nome'].'</td>';
-                                echo '<td>'.$row['email'].'</td>';
-                                echo '<td>'.$row['telefone'].'</td>';
-                                echo '<td><a class="btn btn-outline-danger" href="?delete='.$row['id'].'"><i class="fa-solid fa-trash"></i></a> | <a class="btn btn-outline-secondary" href="index.php?id='.$row['id'].'"><i class="fa-solid fa-pen"></i></a></td>';
-                                echo '</tr>';
-                                $count++;
-                            }
-                        } else {
-                            echo '<tr>';
-                            echo '<td colspan="5">Nenhum registro encontrado</td>';
-                            echo '</tr>';
-                        }
-                    ?>
-                </tr>
-            </tbody>
-        </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous" defer></script>
 </body>
